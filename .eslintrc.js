c1aae6e627b142afaee7390c60b49e5871868c95
andreajl <andreasleppis@gmail.com>
2020-08-27 11:56:40 +0200

    chore: upgrade modules top level

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  extends: [
-    './node_modules/@ardoq/shared-configs/eslint-app',
-  ],
+  extends: ['./node_modules/@ardoq/shared-configs/eslint-app'],
   parserOptions: {
     project: './tsconfig.json',
   },
   env: {
     node: true,
-    webextensions: true
+    webextensions: true,
   },
   rules: {
-    "no-console": 0,
-    "@typescript-eslint/camelcase": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0
-  }
+    'no-console': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
 };
